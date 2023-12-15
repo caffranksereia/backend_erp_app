95f6a4fa01fbe23c85d51c963d5bce965edbc42c
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-15 10:34:44 -0300

    choice for user

---
+++ app-erp/src/choice-user.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Choice() {
+  return (
+    <div>
+      <div>
+            <p>
+                O que deseja?
+            </p>
+      </div>
+      <div>
+            <ul>
+                <li>Login</li>
+                <li>Cadastrar</li>
+                <li>Conhecer o nosso app!</li>
+            </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Choice;
