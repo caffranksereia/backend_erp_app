98750bee55d4be4ef54c8a642d476e8d83784a0d
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-15 10:34:09 -0300

    Signout.tsx

---
+++ app-erp/src/Signout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainHeader from './header';
+
+function SingOut() {
+  return (
+    <div>
+      <h1>SignOut</h1>
+    </div>
+  );
+}
+
+export default SingOut;
