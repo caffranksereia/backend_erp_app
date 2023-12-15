9f74b0321896f28a99403ae6e8e4e0c245d41f57
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-15 10:36:21 -0300

    nothing else better in here

--- app-erp/src/App.test.tsx
+++ app-erp/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './page/Home';
+import App from './Home';
 
 test('renders learn react link', () => {
   render(<App />);
