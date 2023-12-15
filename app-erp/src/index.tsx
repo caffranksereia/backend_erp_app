08fc5e30a00eb7542d4f9fa84335a1080a165173
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-15 10:27:14 -0300

    Name page Home

--- app-erp/src/index.tsx
+++ app-erp/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './page/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
