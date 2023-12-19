0ec9846c0de6108a8ccfcf3abcf7e1f3148778d7
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-19 08:43:54 -0300

    ListViewTodo

---
+++ app-erp/src/components/Todo-Components/ListTodo.Component.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import TodoComponent from './Todo.Component';
+
+function ListViewTodo() {
+  return (
+    <div>
+       
+      <TodoComponent/>
+      <TodoComponent/>
+      <TodoComponent/>
+      <TodoComponent/>
+      <TodoComponent/>
+  
+    </div>
+  );
+}
+
+export default ListViewTodo;
