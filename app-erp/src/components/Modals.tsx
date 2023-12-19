1679e80fb67de48e642bdbce23af9e420e7d6b17
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-19 08:40:43 -0300

    Modals

---
+++ app-erp/src/components/Modals.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Modals() {
+  return (
+    <div>
+        <h1>Titulo</h1>
+        <p>Titulo:</p>
+        <h3>O que fazer?</h3>
+        <p>Estou fazendo!</p>
+        <label>feito</label>
+        <button>Sim</button>
+        <button>Nao</button>
+    </div>
+  );
+}
+
+export default Modals;
