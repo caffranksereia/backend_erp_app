6f8999144d08dbe55f658c7996523ec8bd13d6ec
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-19 08:34:51 -0300

    home moved pages

--- app-erp/src/index.tsx
+++ app-erp/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Home';
-
+import App from './Pages/Home';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
