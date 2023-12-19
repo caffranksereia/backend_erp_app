bea1f3a2f24b750686d22732e8493ae66e68daa9
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-19 08:43:40 -0300

    SingOutComponent

---
+++ app-erp/src/components/SignOut.Component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from '../styles/title.styled';
+
+function SingOutComponent() {
+  return (
+    <div>
+      <Title>You've left</Title>
+    </div>
+  );
+}
+
+export default SingOutComponent;
