ca03148233b51e5a9c094804a216f107d8c21bdd
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-19 09:45:27 -0300

    SignIn

---
+++ app-erp/src/Pages/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignInComponent from '../components/User-Components/SignIn.Component';
+
+function SignIn() {
+  return (
+    <div>
+      <SignInComponent/>
+    </div>
+  );
+}
+
+export default SignIn;
