5af93a2d50447908bd85d0905831cefbeef35292
pastorvelociraptor <circuncisaofabio@icloud.com>
2023-12-19 08:38:01 -0300

    HeaderComponent

---
+++ app-erp/src/components/Header.Component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from '../styles/Container.styled';
+import { Title } from '../styles/title.styled';
+
+function HeaderComponent() {
+  return (
+    <div>
+        <Container>
+            <Title>Bem vindo ao Aplicativo ERP App onde sua vida fica mais organizada!</Title>
+        </Container>
+
+    </div>
+  );
+}
+
+export default HeaderComponent;
