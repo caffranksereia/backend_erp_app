0e352510dc5f5e0f68576459fc23659ee0ecab86
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-05 09:34:31 -0300

    refactore

---
+++ app-erp/src/Pages/index.ts
@@ -0,0 +1,13 @@
+import Dashboard from "./dashboard";
+import Home from "./home";
+import Todo from "./todo";
+import RegisterUser from "./user/register";
+import SignIn from "./user/signIn";
+
+export {
+    Dashboard,
+    Home,
+    Todo,
+    RegisterUser,
+    SignIn
+}
