e22987b5ceed61711ceae581ffe2b4657c329dc5
pastorvelociraptor <circuncisaofabio@icloud.com>
2024-01-05 09:34:43 -0300

    refactore

---
+++ app-erp/src/styles/div/unordered_div/UnorderedListDiv.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const UnorderedListContactDiv = styled.section`
+ display: flex;
+  width:100%;
+  height:100px;
+  justify-content: center;
+  padding: 5px;
+
+`
